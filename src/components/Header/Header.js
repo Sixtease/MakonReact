5a1962368f9f7e98450772de4f3bd7c7539957cd
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-01-13 14:55:02 +0100

    make header look normal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,16 +6,16 @@ export const Header = () => (
     <div className='row'>
         <div className='col-xs-12'>
             <nav className='navbar navbar-default'>
-                <div className='navbar-header'>
-                    <IndexLink to='/' activeClassName='route--active'>
-            audio.Makoň
-          </IndexLink>
-                </div>
                 <ul className='nav navbar-nav'>
                     <li>
+                        <IndexLink to='/' activeClassName='route--active'>
+                            audio.Makoň
+                        </IndexLink>
+                    </li>
+                    <li>
                         <Link to='/o-projektu/' activeClassName='route--active'>
-              o projektu
-            </Link>
+                            o projektu
+                        </Link>
                     </li>
                 </ul>
                 <form className='navbar-form navbar-left' action='/vyhledavani/' method='get'>
@@ -25,7 +25,7 @@ export const Header = () => (
                           className='form-control js-search-input'
                           placeholder='dotaz'
                           name='dotaz'
-            />
+                    />
                     </div>
                     <button type='submit' className='btn btn-default'>hledat</button>
                 </form>
