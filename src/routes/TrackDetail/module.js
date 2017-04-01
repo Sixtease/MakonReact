f5eb71f8ac307d07f561bc2a417dc382a1f4f04f
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-04-02 00:41:23 +0200

    prevent crash by unstarted player

--- src/routes/TrackDetail/module.js
+++ src/routes/TrackDetail/module.js
@@ -103,7 +103,7 @@ export const get_current_word = createSelector(
         const sub = subs[i];
         let start_offset = null;
         let end_offset   = null;
-        if (sub && subs_txt) {
+        if (sub && subs_txt && current_word) {
             start_offset = sub.position;
             end_offset   = sub.position+sub.occurrence.length;
             range.setStart(subs_txt, current_word.start_offset);
