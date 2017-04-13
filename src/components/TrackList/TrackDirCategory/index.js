56c5f3cca38eaeb0df738261074cf83af2cff906
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-04-13 14:16:34 +0200

    style cleanup

--- src/components/TrackList/TrackDirCategory/index.js
+++ src/components/TrackList/TrackDirCategory/index.js
@@ -1,19 +1,15 @@
-import { injectReducer } from '../../../store/reducers';
 import { connect } from 'react-redux';
-import TrackDirCategoryView from './component';
-import { toggle_visible, reducer } from './module';
+import component from './component.js';
+import { toggle_visible, reducer } from './module.js';
 import './style.scss';
 
 const map_dispatch_to_props = {
     toggle_visible,
 };
 
-const map_state_to_props = (state) => {
-    const state_props = {
-        visible: state.track_dir,
-        current_section: (state.track_list || {}).current_section,
-    };
-    return state_props;
-};
+const map_state_to_props = (state) => ({
+    visible: state.track_dir,
+    current_section: (state.track_list || {}).current_section,
+});
 
-export default connect(map_state_to_props, map_dispatch_to_props)(TrackDirCategoryView);
+export default connect(map_state_to_props, map_dispatch_to_props)(component);
