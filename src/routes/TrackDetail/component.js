f6cc4bfbb30f79c4b0cdfabe3d77a42903a2819f
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-04-18 22:24:02 +0200

    fix subsequent track detail loads

--- src/routes/TrackDetail/component.js
+++ src/routes/TrackDetail/component.js
@@ -4,7 +4,10 @@ import audio from 'store/audio.js';
 
 export class TrackDetail extends React.Component {
     state: {
-        subs_offset: 0,
+        subs_offset: {
+            top: 0,
+            left: 0,
+        },
     }
 
     render() {
@@ -15,6 +18,7 @@ export class TrackDetail extends React.Component {
             playback_on, playback_off, force_current_frame, selected_words,
         } = me.props;
         const button_class = 'clickable glyphicon glyphicon-' + (is_playing ? 'pause' : 'play');
+        const subs_offset = me.state ? me.state.subs_offset : {top: 0, left: 0};
         return (<div>
             <h1>{stem}</h1>
             {   is_playing
@@ -41,10 +45,10 @@ export class TrackDetail extends React.Component {
             <p>{selected_words.map((w)=>w.occurrence).join(' ')}</p>
             <div className="subs">
                 <p  ref={(el) => {
-                        this.subs_txt = el ? el.childNodes[0] : null;
-                        this.subs_el  = el;
+                        me.subs_txt = el ? el.childNodes[0] : null;
+                        me.subs_el  = el;
                     }}
-                    onMouseUp={this.props.set_selection}
+                    onMouseUp={me.props.set_selection}
                 >{subs_str}</p>
                 <div className="sub-rects">
                     {current_word.rects.map((rect,i) => (
@@ -52,9 +56,9 @@ export class TrackDetail extends React.Component {
                             key={'sub-rect-'+i}
                             className="sub-rect"
                             style={{
-                                top:    rect.top  - this.state.subs_offset.top + window.scrollY,
-                                left:   rect.left - this.state.subs_offset.left,
-                                width:  rect.right - rect.left,
+                                top:    rect.top    - subs_offset.top + window.scrollY,
+                                left:   rect.left   - subs_offset.left,
+                                width:  rect.right  - rect.left,
                                 height: rect.bottom - rect.top
                             }}
                         ></span>
@@ -69,6 +73,7 @@ export class TrackDetail extends React.Component {
         const {stem} = me.props.params;
         const {set_audio_metadata, sync_current_frame, set_selection} = me.props;
         const src = MP3_BASE + stem + '.mp3';
+        window.scrollTo(0,0);
         set_selection();
         me.audio = audio(src);
         me.audio.addEventListener(
