7855674b02233abf555b79d64cd80687a4dd1db7
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-09-28 20:58:08 +0200

    factored out track detail action handlers

--- src/routes/TrackDetail/module/TrackDetail.js
+++ src/routes/TrackDetail/module/TrackDetail.js
@@ -1,139 +1,16 @@
 import fetch_jsonp from 'fetch-jsonp';
 import query_string from 'query-string';
+import audio from 'store/audio';
 import {
     get_marked_word,
     get_word_rectangles,
     get_selected_words,
 } from './Selectors';
-import audio from 'store/audio';
+import ACTION_HANDLERS from './ActionHandlers';
 
 export const FRAME_RATE = 44100;
 export const frame_to_time = (frame) => frame / FRAME_RATE;
 export const time_to_frame = (time)  => time  * FRAME_RATE;
-
-const ACTION_HANDLERS = {
-    set_subs: (state, action) => ({
-        ...state,
-        subs: action.subs,
-    }),
-    playback_on: (state, action) => {
-        const rv = {
-            ...state,
-            is_playing: true,
-        };
-        const selected_words = action.selected_words/* || get_selected_words({ track_detail:state }) */;
-        if (selected_words.length > 0) {
-            rv.forced_time = selected_words[0].timestamp;
-        }
-        return rv;
-    },
-    playback_off: (state, action) => {
-        return {
-            ...state,
-            is_playing: false,
-        };
-    },
-    clear_forced_time: (state) => ({
-        ...state,
-        forced_time: null,
-    }),
-    set_audio_metadata: (state, action) => ({
-        ...state,
-        frame_cnt: time_to_frame(audio().duration),
-    }),
-    sync_current_time: (state, action) => ({
-        ...state,
-        current_time: action.current_time,
-    }),
-    force_current_time: (state, action) => ({
-        ...state,
-        current_time: action.current_time,
-        forced_time:  action.current_time,
-    }),
-    set_selection: (state, action) => {
-        let new_state = {
-            ...state,
-            selection_start_chunk_index: action.start_chunk_index,
-            selection_end_chunk_index:   action.end_chunk_index,
-            selection_start_icco: action.start_icco, // icco is in-chunk character offset
-            selection_end_icco:   action.  end_icco,
-            is_playing: false,
-        };
-        return new_state;
-    },
-    send_subs: (state, action) => ({
-        ...state,
-        selection_start_chunk_index: null,
-        selection_start_icco:        null,
-        selection_end_chunk_index:   null,
-        selection_end_icco:          null,
-        sending_subs: true,
-        sent_word_rectangles: [
-            ...state.sent_word_rectangles,
-            ...get_word_rectangles(
-                action.words,
-                state.subs,
-                action.subs_chunks/* || get_subs_chunks({track_detail: state}) */,
-            ),
-        ],
-    }),
-    accepted_submission: (state, action) => ({
-        ...state,
-        sending_subs: false,
-        sent_word_rectangles: [],
-        subs: [
-            ...state.subs.slice(
-                0,
-                get_word_index(action.replaced_words[0], state.subs),
-            ),
-            /*
-            ...calculate_word_positions(
-                [
-                    ...action.accepted_words,
-                    ...state.subs.slice(
-                        get_word_index(
-                            // eslint standard/computed-property-even-spacing: [0]
-                            action.replaced_words[
-                                action.replaced_words.length - 1
-                            ],
-                            state.subs,
-                        ) + 1,
-                    ),
-                ],
-                // eslint func-call-spacing: [0]
-                get_word_index   (action.replaced_words[0], state.subs),
-                get_word_position(action.replaced_words[0], state.subs),
-            ),
-            */
-            ...action.accepted_words,
-            ...state.subs.slice(
-                get_word_index(
-                    // eslint standard/computed-property-even-spacing: [0]
-                    action.replaced_words[ action.replaced_words.length - 1 ],
-                    state.subs,
-                ) + 1,
-            ),
-        ],
-    }),
-    failed_submission: (state, action) => ({
-        ...state,
-        sending_subs: false,
-        sent_word_rectangles: [],
-        failed_word_rectangles: [
-            ...state.failed_word_rectangles,
-            ...get_word_rectangles(
-                action.words,
-                state.subs,
-                action.subs_chunks/* || get_subs_chunks({track_detail: state}) */,
-            ),
-        ],
-    }),
-    submission_error: (state, action) => ({
-        ...state,
-        sending_subs: false,
-        sent_word_rectangles: [],
-    }),
-};
 
 const initial_state = {
     subs: [],
@@ -313,6 +190,7 @@ export function playback_off() {
 export function set_audio_metadata() {
     return {
         type: 'set_audio_metadata',
+        frame_cnt: time_to_frame(audio().duration),
     };
 };
 export function sync_current_time(loc, router) {
