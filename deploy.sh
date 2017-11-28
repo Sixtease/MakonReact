10c7e08f6399a319ed1c286178def5f38831ed98
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-11-28 15:50:34 +0100

    deployment script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yarn run deploy:prod || exit 1
+rm -R ~/zdrojaky/python/app_engine/makonreact/static/ ~/zdrojaky/python/app_engine/makonreact/index.html
+mv dist/index.html ~/zdrojaky/python/app_engine/makonreact/index.html || exit 1
+mv dist ~/zdrojaky/python/app_engine/makonreact/static || exit 1
+~/share/install/google/app_engine/google_appengine_1.9.19/appcfg.py --oauth2 update ~/zdrojaky/python/app_engine/makonreact
