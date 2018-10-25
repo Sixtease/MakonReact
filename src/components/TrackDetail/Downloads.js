48fabe0d8476c195a963e53334cbed503d52a629
Jan Oldrich Kruza <sixtease+github@gmail.com>
2018-10-25 15:13:49 +0200

    fix download for mp3, remove for ogg

--- src/components/TrackDetail/Downloads.js
+++ src/components/TrackDetail/Downloads.js
@@ -18,15 +18,12 @@ const component = ({ stem }) => (
         <ul>
             <li>
                 <a
-                    href={AUDIO_BASE + stem + '.mp3'}
+                    href={
+                        'http://commondatastorage.googleapis.com/karel-makon-mp3/' +
+                        stem + '.mp3'
+                    }
                     target='_blank'
                 >{stem}.mp3</a>
-            </li>
-            <li>
-                <a
-                    href={AUDIO_BASE + stem + '.ogg'}
-                    target='_blank'
-                >{stem}.ogg</a>
             </li>
             <li>
                 <a onClick={select_transcription}>
