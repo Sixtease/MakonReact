5fdd0d63962f7f84129359c9dec41ffd29adc524
Jan Oldřich Krůza <jan@sixtease.net>
2025-09-27 18:02:22 +0200

    use https

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 const API_BASE_DEV = 'http://localhost:5000';
-const API_BASE_PRD = 'http://lindat.mff.cuni.cz/services/aligner/makon';
+const API_BASE_PRD = 'https://lindat.mff.cuni.cz/services/aligner/makon';
 const AUDIO_BASE_DEV = 'http://localhost:5000/static/audio/';
-const AUDIO_BASE_PRD = 'http://commondatastorage.googleapis.com/karel-makon-splits/';
+const AUDIO_BASE_PRD = 'https://commondatastorage.googleapis.com/karel-makon-splits/';
 module.exports = {
   AUDIO_FORMATS: [{ mime: 'audio/ogg', suffix: 'ogg' }, { mime: 'audio/mpeg', suffix: 'mp3' }],
   SAMPLE_RATE: 24000,
-  ALIGNER_URL: 'http://lindat.mff.cuni.cz/services/aligner/align',
+  ALIGNER_URL: 'https://lindat.mff.cuni.cz/services/aligner/align',
 };
 if (process.env.NODE_ENV === 'production') {
   module.exports.API_BASE = API_BASE_PRD;
