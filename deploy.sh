bf87c726a7183bc4be372a15f219f8f148af7550
Jan Oldřich Krůza <jan@sixtease.net>
2025-09-27 23:15:16 +0200

    update deploy process

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 
 npm run build || exit 1
 rm -R ~/zdrojaky/python/app_engine/makonreact/static/ ~/zdrojaky/python/app_engine/makonreact/index.html
-mv build/index.html ~/zdrojaky/python/app_engine/makonreact/index.html || exit 1
-mv build ~/zdrojaky/python/app_engine/makonreact/static || exit 1
+cp -R build/* ~/zdrojaky/python/app_engine/makonreact/
 #~/share/install/google/app_engine/google_appengine_1.9.19/appcfg.py --oauth2 update ~/zdrojaky/python/app_engine/makonreact
-gcloud app deploy ~/zdrojaky/python/app_engine/makonreact
+gcloud app deploy ~/zdrojaky/python/app_engine/makonreact --project=makonreact
