54af3f8ca616de8c2e5d603ae5e7d575ae69781a
Jan Oldřich Krůza <jan@sixtease.net>
2025-09-28 11:55:54 +0200

    render by react 18 book

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import AppContainer from './containers/AppContainer';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
-ReactDOM.render(
-  <AppContainer store={store} />,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppContainer store={store} />);
