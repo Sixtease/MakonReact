682ace1c129d28c58e9ef3f65df6416e44af9748
Jan Oldřich Krůza <jan@sixtease.net>
2025-10-20 13:38:25 +0200

    redirect to https

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import AppContainer from './containers/AppContainer';
 import configureStore from './store/configureStore';
 
+if (window.location.hostname === 'radio.makon.cz' && window.location.protocol === 'http:') {
+  window.location.replace(
+    `https://${
+      window.location.host
+    }${
+      window.location.pathname
+    }${
+      window.location.search
+    }${
+      window.location.hash
+    }`
+  );
+}
+
 const store = configureStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<AppContainer store={store} />);
