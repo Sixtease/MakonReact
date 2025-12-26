db93d4afa5d92ebdac6ca2ea3631539afffd8884
Jan Oldřich Krůza <jan@sixtease.net>
2025-12-26 21:14:06 +0100

    add texty endpoints

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,8 @@ const API_BASE_DEV = 'http://localhost:5000';
 const API_BASE_PRD = 'https://lindat.mff.cuni.cz/services/aligner/makon';
 const AUDIO_BASE_DEV = 'http://localhost:5000/static/audio/';
 const AUDIO_BASE_PRD = 'https://commondatastorage.googleapis.com/karel-makon-splits/';
+const TEXTY_BASE_DEV = 'http://localhost:5000/texty';
+const TEXTY_BASE_PRD = 'https://storage.googleapis.com/karel-makon-texty';
 module.exports = {
   AUDIO_FORMATS: [{ mime: 'audio/ogg', suffix: 'ogg' }, { mime: 'audio/mpeg', suffix: 'mp3' }],
   SAMPLE_RATE: 24000,
@@ -10,7 +12,9 @@ module.exports = {
 if (true) {
   module.exports.API_BASE = API_BASE_PRD;
   module.exports.AUDIO_BASE = AUDIO_BASE_PRD;
+  module.exports.TEXTY_BASE = TEXTY_BASE_PRD;
 } else {
   module.exports.API_BASE = API_BASE_DEV;
   module.exports.AUDIO_BASE = AUDIO_BASE_DEV;
+  module.exports.TEXTY_BASE = TEXTY_BASE_DEV;
 }
