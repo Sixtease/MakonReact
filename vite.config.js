210d039ab192a67ee89284fbd7410ccb279d2077
Jan Oldřich Krůza <jan@sixtease.net>
2026-05-13 20:27:51 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'slash-div', 'if-function'],
+      },
+    },
+  },
 });
