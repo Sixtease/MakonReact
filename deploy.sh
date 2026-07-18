e4c468f7b920ddcc4bc33d1bd6cbc9bd7a232ea7
Jan Oldřich Krůza <jan@sixtease.net>
2026-07-18 11:33:44 +0200

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 npm run build || exit 1
-rm -R ~/zdrojaky/python/app_engine/makonreact/static/ ~/zdrojaky/python/app_engine/makonreact/index.html
-cp -R build/* ~/zdrojaky/python/app_engine/makonreact/
+rm -rf \
+  ~/zdrojaky/python/app_engine/makonreact/assets/ \
+  ~/zdrojaky/python/app_engine/makonreact/static/ \
+  ~/zdrojaky/python/app_engine/makonreact/index.html \
+  ~/zdrojaky/python/app_engine/makonreact/asset-manifest.json
+cp -R dist/* ~/zdrojaky/python/app_engine/makonreact/
 #~/share/install/google/app_engine/google_appengine_1.9.19/appcfg.py --oauth2 update ~/zdrojaky/python/app_engine/makonreact
 gcloud app deploy ~/zdrojaky/python/app_engine/makonreact --project=makonreact
